3e42a109f0c39aeba3bf829727e6995b2174523a
Raza <razabinashraf@gmail.com>
2020-05-10 23:29:03 +0530

    Initil commit

---
+++ agent_portal.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="asset/stylesheet.css">
+    <title>Masafi water dist. co.</title>
+</head>
+<body>
+<main>
+<header>
+    <div class="header--heading">
+        Masafi Water
+    </div>
+    <nav>
+        <div class="nav--user-toggle">
+            <div class="nav--user-toggle-info">You have loged in as ADMIN</div>
+        </div>
+    </nav>
+</header>
+<section>
+<div class="container">
+<div class="portal--option-box">
+    <ul class="portal--options">
+        <li>
+            <div><i class="fa fa-bars" style="font-size: 48px;"></i></div><br/>
+            <div><p>Report</p></div>
+        </li>
+        <li>
+            <div><i class="fa fa-plus-square" style="font-size: 48px;"></i></div><br/>
+            <div><p>Add Customer</p></div>
+        </li>
+        <li>
+            <div><i class="fa fa-shopping-cart" style="font-size: 48px;"></i></div><br/>
+            <div><p>Transaction</p></div>
+        </li>
+    </ul>
+</div>
+</div>
+</section>
+
+</main>
+<!--<script src="asset/app.js"></script>-->
+</body>
+</html>
---
+++ index_admin.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="asset/stylesheet.css">
+    <title>Water dist.</title>
+</head>
+<body>
+<main>
+<header>
+    <div class="header--heading">
+        Masafi Water
+    </div>
+    <nav>
+        <div class="nav--user-toggle">
+            <a href="index.html"><div class="nav--user-toggle-link">AGENT</div></a>
+            <a href="index_admin.html" class="active"><div class="nav--user-toggle-link">ADMIN</div></a>
+        </div>
+    </nav>
+</header>
+<section>
+<div class="container">
+<div class="login-box">
+    <div class="login--icon">
+        <img src="asset/img/car.png" alt="">
+    </div>
+    <form>
+        <div class="login--input"><input class="login--input-types" type="text" placeholder="Admin Id" id="agentid" name="agentid" required></div>
+        <div class="login--input"><input class="login--input-types" type="password" placeholder="password" id="password" name="password" required></div>
+        <div class="login--input"><button class="login--input-types" id="submit" name="submit">Submit</button></div>
+    </form>
+</div>
+</div>
+</section>
+
+</main>
+<!--<script src="asset/app.js"></script>-->
+</body>
+</html>
